eecfeb3a04e67f5b0256eea837e067cc22c26d99
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-11-23 09:43:17 -0600

    se agrego el build  para  deploy

---
+++ build/precache-manifest.984d5aaf93e4dd669eb514cb48fd2fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b734605baf7873a8c6e7",
+    "url": "/static/js/main.b734605b.chunk.js"
+  },
+  {
+    "revision": "4f7d43b1fd97fcce8299",
+    "url": "/static/js/1.4f7d43b1.chunk.js"
+  },
+  {
+    "revision": "b734605baf7873a8c6e7",
+    "url": "/static/css/main.3088ac6a.chunk.css"
+  },
+  {
+    "revision": "8f70f8208cdb088e89005cbe3521be59",
+    "url": "/index.html"
+  }
+];
